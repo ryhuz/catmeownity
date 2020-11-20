c80731a2dbc9af6cffaf2dd255eff6536c6e7d82
hfzgfr <hfzgfr@gmail.com>
2020-11-20 13:52:38 +0800

    added comments on both  user and public

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -3,18 +3,18 @@ const router = require("express").Router();
 const Cat = require("../models/cat.model.js");
 const District = require("../models/district.model");
 
+/* get cat data */
 router.get('/', async (req, res) => {
  try {
   let cat = await Cat.find(req.params);
   return res.status(200).json({
    message: "Successfully fetched cats!",
   })
-
  } catch (error) { 
-  res.status(400).json({ message: "Problem fetching data!"})
+  res.status(400).json({ message: "Problem fetching cats data!"})
  }
 });
-
+/* get district data */
 router.get('/district', async (req, res) => {
  try {
   let district = District.find(req.params)
