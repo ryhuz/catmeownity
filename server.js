741c65fa7c2cbbabaf0e9d12f9c4ec99418f50b5
Mohd Saiful <mohd.saiful@live.com>
2020-11-20 12:02:26 +0800

    created the mongodb cloud connection

--- server.js
+++ server.js
@@ -1,36 +1,16 @@
-ROUTES
-/public          -- view cats, view location
-/user            -- login, register
-/auth/cats       -- create, update, delete, mark fed, mark potential duplicate
-/auth/locations  -- create, update, delete
-/auth/user       -- edit user, add tracked locations
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-SCHEMA
-Cats
-----------
-ID
-Names []
-Locations [{ District(Model), Street Name, remarks}] ----Street Name : xxxxxxxxx St/Dr/Ave xxxx
-Breed
-Colour
-Descriptions(reviews) [] -
-Missing - boolean
-Fed [ latest first - {Time, fed by: User(model)}]
-Photos []
-Potential Duplicate {Boolean, Cat(Model)}
 
-District
-----------
-Name
-Locality: nsew (enum)
-Estates: model
+require('./lib/connection');
 
-Users
-----------
-Email - unique
-Password
-Full name
-Primary location
-Tracked locations
-Favourite cats
-Admin?
+/* middleware */
+
+
+/* routes */
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Connected on ${process.env.PORT}`);
+});
