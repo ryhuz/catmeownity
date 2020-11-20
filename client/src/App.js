43e085cab00eafcf79cc132cb1baf999b1ddf53e
ryhuz <shawn.wee90@hotmail.com>
2020-11-20 14:39:54 +0800

    starting on front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Link, NavLink, Route, Switch } from 'react-router-dom'
+import Home from './Home';
+import { Button } from '@material-ui/core'
 
 function App() {
+
+  function checkLoggedIn() {
+    return false;
+  }
+
+  function navLoggedIn() {
+
+  }
+  function navNotLoggedIn() {
+    return
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      {checkLoggedIn() ? navLoggedIn() : navNotLoggedIn()}
+      <Button variant="contained">Default</Button>
+
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
--- routes/public.routes.js
+++ routes/public.routes.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const Cat = require("../models/cat.model.js");
 const District = require("../models/district.model");
 
+/* get cat data */
 router.get('/', async (req, res) => {
  try {
   let cat = await Cat.find(req.params);
@@ -11,10 +12,11 @@ router.get('/', async (req, res) => {
   })
 
  } catch (error) { 
-  res.status(400).json({ message: "Problem fetching data!"})
+  res.status(400).json({ message: "Problem fetching cats data!"})
  }
 });
 
+/* get district data */
 router.get('/district', async (req, res) => {
  try {
   let district = District.find(req.params)
