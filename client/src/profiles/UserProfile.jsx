116e3e909a47805c0d411c382a01fc535e50aab1
hfzgfr <hfzgfr@gmail.com>
2020-11-23 20:44:26 +0800

    initial changes to profile page

--- client/src/profiles/UserProfile.jsx
+++ client/src/profiles/UserProfile.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Row, Col, Image, Table } from 'react-bootstrap';
+import { Card, Col, Container, Image, Nav, Row, Table } from 'react-bootstrap';
 import { NavLink, useParams } from 'react-router-dom';
 import Axios from 'axios'
 
@@ -33,7 +33,7 @@ const UserProfile = () => {
           <Row className="p-3">
             <Col>
               <div>
-                <Image className="mx-auto" width="100%" src="http://placekitten.com/300/300" roundedCircle />
+                <Image className="mx-auto p-4" width="100%" src="http://placekitten.com/300/300" roundedCircle />
               </div>
               {/* Set link to edit profile page here */}
               <NavLink to='/'><span className="btn btn-dark btn-block">Edit Profile</span></NavLink>
@@ -45,23 +45,23 @@ const UserProfile = () => {
                 <Table borderless size="lg">
                   <tbody>
                     <tr>
-                      <td>Name: </td>
+                      <td><strong>Name: </strong></td>
                       <td> {user.user.name} </td>
                     </tr>
                     <tr>
-                      <td>Email: </td>
+                      <td><strong>Email: </strong></td>
                       <td> {user.user.email}  </td>
                     </tr>
                     <tr>
-                      <td>Home Location: </td>
+                      <td><strong>Home Location: </strong></td>
                       <td> {user.user.location.street}  </td>
                     </tr>
                     <tr>
-                      <td>Other tracked locations: </td>
+                      <td><strong>Other tracked locations: </strong></td>
                       <td> This can be at the bottom  </td>
                     </tr>
                     <tr>
-                      <td>Fave cats: </td>
+                      <td><strong>Fave cats: </strong></td>
                       <td> This can be at the bottom </td>
                     </tr>
                   </tbody>
@@ -71,6 +71,25 @@ const UserProfile = () => {
           </Row>
         </Container>
       }
+        <Container className="d-flex flex-row border border-dark mt-4">
+          <Row className="p-3">
+            <Col>
+                <Table>
+                  <tbody>
+                    <tr>
+                      <td><strong>Tracked locations: </strong></td>
+                      <td></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Fave Cats: </strong></td>
+                      <td></td>
+                    </tr>
+                  </tbody>
+                </Table>
+            </Col>
+          </Row>
+        </Container>
+
     </>
   )
 }
