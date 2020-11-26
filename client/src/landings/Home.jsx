4ba819eb6bad3838a1cd3bd88184714c22827ea5
ryhuz <shawn.wee90@hotmail.com>
2020-11-26 19:47:23 +0800

    populated trivia

--- client/src/landings/Home.jsx
+++ client/src/landings/Home.jsx
@@ -4,6 +4,47 @@ import { NavLink } from 'react-router-dom'
 import '../App.css'
 
 function Home() {
+    let trivia = [
+        {
+            header: "What are cats' whiskers for?",
+            trivia: `Cats use their whiskers to navigate through the world. They use them to help determine if 
+            they can fit in a small space because their whiskers are approximately the same width as 
+            their body. This is why you shouldn’t cut your cat’s whiskers.`
+        },
+        {
+            header: "Wild cat?",
+            trivia: `Cat’s share 95.6 percent of their genome with tigers and demonstrate several of the same behaviors.`
+        },
+        {
+            header: "Play time!",
+            trivia: `Cats with a question mark-shaped tail are feeling playful.`
+        },
+        {
+            header: "Why are you hitting me?",
+            trivia: `A cat batting you with paws (and not claws) is playing and not attacking.`
+        },
+        {
+            header: "How many?",
+            trivia: `A group of cats can be called a “clowder,” “clutter,” “glaring” and “pounce.”`
+        },
+        {
+            header: "Meow",
+            trivia: `Cats have as many as 100 different vocalizations while dogs only have 10.`
+        },
+        {
+            header: "Danger, Will Robinson",
+            trivia: `Unlike dogs, beware a cat that is wagging their tail!`
+        },
+        {
+            header: "Bath time",
+            trivia: `Grooming themselves helps cats relax, stimulate their blood flow and regulate their body temperature.`
+        },
+        {
+            header: "Toilet time",
+            trivia: `Cats prefer to have a litter box for themselves so have one for each of your kitties.`
+        },
+
+    ]
     return (
         <>
             <Jumbotron id="jumbo" className='text-center jumbotop'>
@@ -23,42 +64,15 @@ function Home() {
                 <Container>
                     <h4 className="text-center p-4">Learn more about cats</h4>
                     <Carousel>
-                        <Carousel.Item>
-                            <img
-                                id="trivia1"
-                                className="d-block w-100"
-                                src="http://placehold.it/800x300"
-                                alt="First slide"
-                            />
-                            <Carousel.Caption bg="dark" className="text-dark">
-                                <h3>First Cat Trivia</h3>
-                                <p>Cats are awesome</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src="http://placehold.it/800x300"
-                                alt="Third slide"
-                            />
-
-                            <Carousel.Caption>
-                                <h3>Second Cat Trivia</h3>
-                                <p>Not all cats like to be pet</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img
-                                className="d-block w-100"
-                                src="http://placehold.it/800x300"
-                                alt="Third slide"
-                            />
-
-                            <Carousel.Caption>
-                                <h3>Third Cat Trivia</h3>
-                                <p>Don't leave the food lying around</p>
-                            </Carousel.Caption>
-                        </Carousel.Item>
+                        {trivia.map((item, index) => (
+                            <Carousel.Item>
+                                <img id={`trivia${index}`} className="d-block w-100" src="https://images.pexels.com/photos/35888/amazing-beautiful-breathtaking-clouds.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+                                <Carousel.Caption>
+                                    <h3>{item.header}</h3>
+                                    <p>{item.trivia}</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        ))}
                     </Carousel>
                 </Container>
             </div>
